7aa85c2dcbd98b9c819203db84df125ca588b86a
dardonosti <dardonostiok@gmail.com>
2020-02-21 19:19:56 -0300

    gestión_cliente: completo

--- gpro-desktop-windows/UserControls/ucDashboard.Designer.cs
+++ gpro-desktop-windows/UserControls/ucDashboard.Designer.cs
@@ -35,13 +35,14 @@
       this.metroTile4 = new MetroFramework.Controls.MetroTile();
       this.metroTile5 = new MetroFramework.Controls.MetroTile();
       this.metroTile6 = new MetroFramework.Controls.MetroTile();
+      this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
       this.SuspendLayout();
       // 
       // mtClientes
       // 
       this.mtClientes.ActiveControl = null;
       this.mtClientes.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.mtClientes.Location = new System.Drawing.Point(28, 27);
+      this.mtClientes.Location = new System.Drawing.Point(100, 66);
       this.mtClientes.Name = "mtClientes";
       this.mtClientes.Size = new System.Drawing.Size(130, 119);
       this.mtClientes.TabIndex = 0;
@@ -53,7 +54,7 @@
       // 
       this.metroTile1.ActiveControl = null;
       this.metroTile1.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.metroTile1.Location = new System.Drawing.Point(28, 164);
+      this.metroTile1.Location = new System.Drawing.Point(100, 203);
       this.metroTile1.Name = "metroTile1";
       this.metroTile1.Size = new System.Drawing.Size(130, 119);
       this.metroTile1.TabIndex = 0;
@@ -64,7 +65,7 @@
       // 
       this.metroTile2.ActiveControl = null;
       this.metroTile2.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.metroTile2.Location = new System.Drawing.Point(180, 27);
+      this.metroTile2.Location = new System.Drawing.Point(252, 66);
       this.metroTile2.Name = "metroTile2";
       this.metroTile2.Size = new System.Drawing.Size(130, 119);
       this.metroTile2.TabIndex = 0;
@@ -75,7 +76,7 @@
       // 
       this.metroTile3.ActiveControl = null;
       this.metroTile3.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.metroTile3.Location = new System.Drawing.Point(180, 164);
+      this.metroTile3.Location = new System.Drawing.Point(252, 203);
       this.metroTile3.Name = "metroTile3";
       this.metroTile3.Size = new System.Drawing.Size(130, 119);
       this.metroTile3.TabIndex = 0;
@@ -86,7 +87,7 @@
       // 
       this.metroTile4.ActiveControl = null;
       this.metroTile4.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.metroTile4.Location = new System.Drawing.Point(28, 304);
+      this.metroTile4.Location = new System.Drawing.Point(100, 343);
       this.metroTile4.Name = "metroTile4";
       this.metroTile4.Size = new System.Drawing.Size(282, 68);
       this.metroTile4.TabIndex = 0;
@@ -97,7 +98,7 @@
       // 
       this.metroTile5.ActiveControl = null;
       this.metroTile5.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.metroTile5.Location = new System.Drawing.Point(332, 27);
+      this.metroTile5.Location = new System.Drawing.Point(404, 66);
       this.metroTile5.Name = "metroTile5";
       this.metroTile5.Size = new System.Drawing.Size(130, 119);
       this.metroTile5.TabIndex = 0;
@@ -108,17 +109,33 @@
       // 
       this.metroTile6.ActiveControl = null;
       this.metroTile6.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.metroTile6.Location = new System.Drawing.Point(332, 164);
+      this.metroTile6.Location = new System.Drawing.Point(404, 203);
       this.metroTile6.Name = "metroTile6";
       this.metroTile6.Size = new System.Drawing.Size(130, 119);
       this.metroTile6.TabIndex = 0;
       this.metroTile6.Text = "Lorem Ipsum";
       this.metroTile6.UseSelectable = true;
       // 
+      // metroPanel1
+      // 
+      this.metroPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+      this.metroPanel1.HorizontalScrollbarBarColor = true;
+      this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+      this.metroPanel1.HorizontalScrollbarSize = 10;
+      this.metroPanel1.Location = new System.Drawing.Point(0, 0);
+      this.metroPanel1.Name = "metroPanel1";
+      this.metroPanel1.Size = new System.Drawing.Size(71, 775);
+      this.metroPanel1.TabIndex = 7;
+      this.metroPanel1.UseCustomBackColor = true;
+      this.metroPanel1.VerticalScrollbarBarColor = true;
+      this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+      this.metroPanel1.VerticalScrollbarSize = 10;
+      // 
       // ucDashboard
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.metroPanel1);
       this.Controls.Add(this.metroTile6);
       this.Controls.Add(this.metroTile3);
       this.Controls.Add(this.metroTile4);
@@ -127,7 +144,7 @@
       this.Controls.Add(this.metroTile2);
       this.Controls.Add(this.mtClientes);
       this.Name = "ucDashboard";
-      this.Size = new System.Drawing.Size(712, 413);
+      this.Size = new System.Drawing.Size(900, 742);
       this.ResumeLayout(false);
 
     }
@@ -141,5 +158,6 @@
     private MetroFramework.Controls.MetroTile metroTile4;
     private MetroFramework.Controls.MetroTile metroTile5;
     private MetroFramework.Controls.MetroTile metroTile6;
+    private MetroFramework.Controls.MetroPanel metroPanel1;
   }
 }
